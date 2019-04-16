d864a0a03e36f036730c340a40bb8f484bea8b4d
jdm79 <james.malvern@gmail.com>
2019-04-15 20:49:05 -0500

    first commit: project 4

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by James Daniel Malvern on 15/04/2019.
+//  Copyright © 2019 Malvernation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
