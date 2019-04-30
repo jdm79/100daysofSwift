568c9227f6247f808de61241a91db8e9aaf1bfab
jdm79 <james.malvern@gmail.com>
2019-04-30 14:04:14 -0500

    refactor: changes the trash icon, gives a title to app

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -15,10 +15,15 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Shopping List"
+        
+        startApp()
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
         
-        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteItems))
     }
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shoppingList.count
@@ -28,6 +33,11 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
         cell.textLabel?.text = shoppingList[indexPath.row]
         return cell
+    }
+    
+    func startApp() {
+        shoppingList.removeAll(keepingCapacity: true)
+        tableView.reloadData()
     }
     
     @objc func addItem() {
@@ -48,11 +58,16 @@ class ViewController: UITableViewController {
     func submit(_ item: String) {
         
             shoppingList.insert(item, at: 0)
+        
             
             let indexPath = IndexPath(row: 0, section: 0)
             tableView.insertRows(at: [indexPath], with: .automatic)
             
             return
-        }
     }
+    
+    @objc func deleteItems() {
+        startApp()
+    }
+}
 
