04773b91d96b024defa47558b9230ec91b32b555
jdm79 <james.malvern@gmail.com>
2019-04-30 13:14:38 -0500

    feat: adds textfield to add items, displays contents of shoppinglist

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        
+        
     }
-
-
-}
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    @objc func addItem() {
+        
+        let ac = UIAlertController(title: "Add item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Add", style: .default) {
+            [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.submit(item)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ item: String) {
+        
+            shoppingList.insert(item, at: 0)
+            
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+            
+            return
+        }
+    }
 
