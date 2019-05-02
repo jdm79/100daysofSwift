199bbac724c7130bd5aa9e2846a71ac3659a45fb
jdm79 <james.malvern@gmail.com>
2019-05-01 22:31:22 -0500

    first commit, creates table view controller, navigation controller and tab bar controller

---
+++ Project7Codable/Project7Codable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7Codable
+//
+//  Created by James Daniel Malvern on 01/05/2019.
+//  Copyright © 2019 Malvernation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
