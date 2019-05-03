79a871428fe3bf5489047c4930db33add972b2ee
jdm79 <james.malvern@gmail.com>
2019-05-02 22:07:29 -0500

    feat: creates new function which fetches the data, creates a reload button which makes fresh calls and repopulates the table view, more robust error handling with an error function

--- Project7Codable/Project7Codable/ViewController.swift
+++ Project7Codable/Project7Codable/ViewController.swift
@@ -14,13 +14,38 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(reload))
+    
+        fetchData()
+    }
+    
+    func fetchData() {
+        
+        let urlString: String
+
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            //            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            //            urlString = "https://api.whitehouse.gov/v1/petitins.json?signatureCountFloor=10000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
+        showError()
+    }
+    
+    func showError() {
+        let message = "There was a problem loading the feed; please check your connection and try again"
+        let ac = UIAlertController(title: "Loading Error", message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data) {
@@ -49,6 +74,14 @@ class ViewController: UITableViewController {
         vc.detailItem = petitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
-
+    
+    @objc func reload() {
+        fetchData()
+        let message = "Fetching latest petitions"
+        let ac = UIAlertController(title: "New Fetch Call", message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        tableView.reloadData()
+    }
 }
 
